1f54e9c4c5c98264c2b9cfa998d58c4fcecb683c
nico-b <baron.nicolas@gmail.com>
2013-08-12 00:31:10 +0200

    Week 5 answer

--- Week_5/dijkstra.rb
+++ Week_5/dijkstra.rb
@@ -1,6 +1,101 @@
 #!/usr/bin/env ruby
-require 'algorithms'
+require 'set'
 
-def read_graph(file)
+#returns  "edges : [[end1, length1], [end2, length2]] and an ordered array of vertices
+def read_file(file)
+  puts "Start read graph"
+
+  edges = {}
+  vertices = []
+
+  IO.foreach(file) {|line|
+
+    split_line = line.split(' ')
+
+    start_vertex = split_line[0].to_i
+    vertices << start_vertex
+
+    for i in 1..split_line.length-1
+      split_el = split_line[i].split(',')
+
+      if edges[start_vertex]
+        edges[start_vertex] << ([split_el[0].to_i, split_el[1].to_i])
+      else
+        edges[start_vertex] = [[split_el[0].to_i, split_el[1].to_i]]
+      end
+
+    end
+  }
+
+  puts "End read graph"
+
+  return vertices, edges
+end
+
+#Implem based on Wikipedia pseudo_code : http://en.wikipedia.org/wiki/Dijkstra's_algorithm
+def dijkstra(file, source_node)
+  vertices, edges = read_file(file)
+
+  dist = {}
+
+  q = Set.new
+
+  vertices.each do |v|
+    dist[v] = 1000000
+    q << v
+  end
+
+  dist[source_node] = 0
+
+  while not q.empty?
+
+    min = 1000000
+
+    u = nil
+
+    q.each do |vertex|
+      if dist[vertex] < min
+        min = dist[vertex]
+        u = vertex
+      end
+    end
+
+    q.delete?(u)
+
+    break if dist[u] == 1000000
+
+    if edges[u]
+      edges[u].each do |neighbour_and_dist|
+        v = neighbour_and_dist[0]
+        dist_u_v = neighbour_and_dist[1]
+        greedy_score = dist[u] + dist_u_v
+        if greedy_score < dist[v]
+          dist[v] = greedy_score
+        end
+      end
+    end
+
+  end
+
+  return dist
+
+end
+
+def compute(file, start_node, end_nodes)
+
+  puts "Start algorithm"
+  start = Time.now
+
+  shortest_path_distances = dijkstra(file, start_node)
+
+  required_s_path_distances = []
+
+  end_nodes.each { |end_node|
+    required_s_path_distances << shortest_path_distances[end_node]
+  }
+
+  puts "runtime: #{Time.now - start}"
+
+  return required_s_path_distances.join(',')
 
 end
--- Week_5/dijkstra_test.rb
+++ Week_5/dijkstra_test.rb
@@ -4,28 +4,35 @@ require_relative 'dijkstra'
 class SccTest < Test::Unit::TestCase
 
   def test_read_file
-    edges = read_file('dijkstra_test_case_1.txt')
-    assert_equal 8, edges.length
-    assert_equal [1,2,5], edges[0]
-    assert_equal [3,1,1], edges[5]
+    vertices, edges = read_file('dijkstra_test_case_1.txt')
+    assert_equal 4, edges.keys.length
+    assert_equal [[2,1], [3,4]], edges[1]
+    assert_equal [[4,3], [1,4], [2,2]], edges[3]
   end
 
   def test_case_1
-    end_nodes_for_shortest_path = [4]
+    end_nodes_for_shortest_path = [1,2,3,4]
     shortest_paths = compute('dijkstra_test_case_1.txt', 1, end_nodes_for_shortest_path)
-    assert_equal 2, shortest_paths[0]
+    assert_equal '0,1,3,6', shortest_paths
   end
 
   def test_case_2
-    end_nodes_for_shortest_path = [7]
+    end_nodes_for_shortest_path = [1,2,3,4,5]
     shortest_paths = compute('dijkstra_test_case_2.txt', 1, end_nodes_for_shortest_path)
-    assert_equal 5, shortest_paths[0]
+    assert_equal '0,10,50,30,60', shortest_paths
   end
 
   def test_case_3
     end_nodes_for_shortest_path = [5]
     shortest_paths = compute('dijkstra_test_case_3.txt', 13, end_nodes_for_shortest_path)
-    assert_equal 26, shortest_paths[0]
+    assert_equal '26', shortest_paths
   end
+
+  def test_case_exo
+    end_nodes_for_shortest_path = [7,37,59,82,99,115,133,165,188,197]
+    shortest_paths = compute('dijkstraData.txt', 1, end_nodes_for_shortest_path)
+    assert_equal '2599,2610,2947,2052,2367,2399,2029,2442,2505,3068', shortest_paths
+  end
+
 
 end
